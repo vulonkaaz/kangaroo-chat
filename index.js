fcdcdcb1c54f9c00ed85d093905bac0e548b2c8a
vulonkaaz <7442677+vulonkaaz@users.noreply.github.com>
2023-07-18 15:59:52 +0200

    cors

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 //const path = require('path');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const router = require('./src/router');
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use(router);
 app.use((_,res) => {res.status(404).send("route not defined")});
